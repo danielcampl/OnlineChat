f5218cd0b07671e58e050580764ffd9c0529343a
daniel <danielcampelo159@gmail.com>
2024-02-14 00:59:46 -0300

    Vite config reconfiguration testing for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+        'node:path',
+        'node:https',
+        'node:http',
+        'node:zlib',
+      ]
+    }
+  }
 })
+
